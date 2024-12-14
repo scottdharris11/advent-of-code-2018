03606fff751b7d7ce690bca8f3b505e90d53e260
scottdharris <scottdharris11@yahoo.com>
2024-12-13 19:34:29 -0500

    day 2, part 1

--- day02.py
+++ day02.py
@@ -5,7 +5,17 @@ from utilities.runner import runner
 @runner("Day 2", "Part 1")
 def solve_part1(lines: list[str]):
     """part 1 solving function"""
-    return 0
+    appear2 = 0
+    appear3 = 0
+    for line in lines:
+        cnt_by_letters = {}
+        for c in line:
+            cnt_by_letters[c] = cnt_by_letters.get(c, 0) + 1
+        if 2 in cnt_by_letters.values():
+            appear2 += 1
+        if 3 in cnt_by_letters.values():
+            appear3 += 1
+    return appear2 * appear3
 
 @runner("Day 2", "Part 2")
 def solve_part2(lines: list[str]):
@@ -14,11 +24,17 @@ def solve_part2(lines: list[str]):
 
 # Data
 data = read_lines("input/day02/input.txt")
-sample = """""".splitlines()
+sample = """abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab""".splitlines()
 
 # Part 1
-assert solve_part1(sample) == 0
-assert solve_part1(data) == 0
+assert solve_part1(sample) == 12
+assert solve_part1(data) == 5704
 
 # Part 2
 assert solve_part2(sample) == 0
