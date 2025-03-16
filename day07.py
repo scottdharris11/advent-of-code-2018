131993b92792928fe10dc411f19fe5625e73c04f
scottdharris <scottdharris11@yahoo.com>
2025-03-16 05:25:47 -0400

    day 7 part 2

--- day07.py
+++ day07.py
@@ -21,9 +21,30 @@ def solve_part1(lines: list[str]) -> str:
     return order
 
 @runner("Day 7", "Part 2")
-def solve_part2(lines: list[str]):
+def solve_part2(lines: list[str], workers: int, duration: int):
     """part 2 solving function"""
-    return 0
+    queue, steps, depends = parse_steps(lines)
+    seconds = 0
+    inprogress = {}
+    while len(queue) > 0 or len(inprogress) > 0:
+        remove = []
+        for i, d in inprogress.items():
+            if d == seconds:
+                for s in depends.get(i, []):
+                    prereqs = steps.get(s, [])
+                    prereqs.remove(i)
+                    if len(prereqs) == 0:
+                        queue.append(s)
+                    steps[s] = prereqs
+                remove.append(i)
+        for r in remove:
+            del inprogress[r]
+        while len(queue) > 0 and len(inprogress) < workers:
+            queue.sort(reverse=True)
+            step = queue.pop()
+            inprogress[step] = seconds + duration + (ord(step) - ord('A') + 1)
+        seconds += 1
+    return seconds - 1
 
 def parse_steps(lines: list[str]) -> tuple[list[str],dict[str,list[str]],dict[str,list[str]]]:
     """parse the step dependencies from the input"""
@@ -62,5 +83,5 @@ assert solve_part1(sample) == "CABDFE"
 assert solve_part1(data) == "LFMNJRTQVZCHIABKPXYEUGWDSO"
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample, 2, 0) == 15
+assert solve_part2(data, 5, 60) == 1180
