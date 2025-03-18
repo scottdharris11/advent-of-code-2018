5a4b484ebfd7a24429f7a3981df8ee0df26e2e12
scottdharris <scottdharris11@yahoo.com>
2025-03-18 14:27:37 -0400

    day 9, part 2

--- day09.py
+++ day09.py
@@ -1,10 +1,25 @@
 """utility imports"""
-from utilities.data import read_lines
 from utilities.runner import runner
 
 @runner("Day 9", "Part 1")
 def solve_part1(players: int, last: int) -> int:
     """part 1 solving function"""
+    return play(players, last)
+
+@runner("Day 9", "Part 2")
+def solve_part2(players: int, last: int) -> int:
+    """part 2 solving function"""
+    return play(players, last * 100)
+
+class Marble:
+    """marble structure"""
+    def __init__(self, identifier: int):
+        self.identifier = identifier
+        self.next = None
+        self.prev = None
+
+def play(players: int, last: int) -> int:
+    """play the marble game"""
     current = Marble(0)
     current.next = current
     current.prev = current
@@ -34,22 +49,6 @@ def solve_part1(players: int, last: int) -> int:
         player = (player + 1) % players
     return max(scores)
 
-@runner("Day 9", "Part 2")
-def solve_part2(players: int, marbles: int) -> int:
-    """part 2 solving function"""
-    return 0
-
-class Marble:
-    """marble structure"""
-    def __init__(self, identifier: int):
-        self.identifier = identifier
-        self.next = None
-        self.prev = None
-
-# Data
-data = read_lines("input/day09/input.txt")
-sample = """""".splitlines()
-
 # Part 1
 assert solve_part1(9, 25) == 32
 assert solve_part1(10, 1618) == 8317
@@ -60,5 +59,4 @@ assert solve_part1(30, 5807) == 37305
 assert solve_part1(426, 72058) == 424112
 
 # Part 2
-assert solve_part2(9, 25) == 0
-assert solve_part2(426, 72058) == 0
+assert solve_part2(426, 72058) == 3487352628
