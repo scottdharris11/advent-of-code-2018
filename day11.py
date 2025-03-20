7f0cd466ea6eeccc2bfc19481159828e504d6900
scottdharris <scottdharris11@yahoo.com>
2025-03-20 09:44:06 -0400

    day 11 part 2, correct but slow

--- day11.py
+++ day11.py
@@ -19,9 +19,39 @@ def solve_part1(serial: int) -> str:
     return point
 
 @runner("Day 11", "Part 2")
-def solve_part2(serial: int) -> int:
+def solve_part2(serial: int) -> str:
     """part 2 solving function"""
-    return 0
+    grid = build_grid(serial)
+    largest = None
+    point = None
+    for s in range(1,301):
+        values = {}
+        for y in range(300-s+1):
+            for x in range(300-s+1):
+                p = 0
+                if (x-1,y) in values:
+                    p = values[(x-1,y)]
+                    for yo in range(s):
+                        p -= grid[y+yo][x-1]
+                    for yo in range(s):
+                        p += grid[y+yo][x+s-1]
+                    values[(x,y)] = p
+                elif (x,y-1) in values:
+                    p = values[(x,y-1)]
+                    for xo in range(s):
+                        p -= grid[y-1][x+xo]
+                    for xo in range(s):
+                        p += grid[y+s-1][x+xo]
+                    values[(x,y)] = p
+                else:
+                    for yo in range(s):
+                        for xo in range(s):
+                            p += grid[y+yo][x+xo]
+                    values[(x,y)] = p
+                if largest is None or p > largest:
+                    largest = p
+                    point = f"{x+1},{y+1},{s}"
+    return point
 
 def build_grid(serial: int) -> list[list[int]]:
     """build power grid"""
@@ -53,5 +83,6 @@ assert solve_part1(42) == "21,61"
 assert solve_part1(7857) == "243,16"
 
 # Part 2
-assert solve_part2(18) == 0
-assert solve_part2(7857) == 0
+assert solve_part2(18) == "90,269,16"
+assert solve_part2(42) == "232,251,12"
+assert solve_part2(7857) == "231,227,14"
