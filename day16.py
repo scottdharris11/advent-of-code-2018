1197cad866d6943379474386f350000bdd65755a
scottdharris <scottdharris11@yahoo.com>
2025-04-25 11:04:48 -0400

    day 16, part 2

--- day16.py
+++ day16.py
@@ -22,7 +22,25 @@ def solve_part1(lines: list[str]) -> int:
 @runner("Day 16", "Part 2")
 def solve_part2(lines: list[str]) -> int:
     """part 2 solving function"""
-    return 0
+    samples, ops = parse_input(lines)
+    poperations = {}
+    for s in samples:
+        before = parse_integers(s[0][9:len(s[0])-1], ",")
+        expected = parse_integers(s[2][9:len(s[2])-1], ",")
+        operation = s[1]
+        for f in [addr,addi,mulr,muli,banr,bani,borr,bori,setr,seti,gtir,gtri,gtrr,eqir,eqri,eqrr]:
+            opcd = match(f, before, expected, operation)
+            if opcd >= 0:
+                opcds = poperations.get(f, set())
+                opcds.add(opcd)
+                poperations[f] = opcds
+    operations = determine_opcodes(poperations)
+    registers = {0:0, 1:0, 2:0, 3:0}
+    for op in ops:
+        opcode, a, b, c, *_ = parse_integers(op, " ")
+        func = operations[opcode]
+        func(registers, a, b, c)
+    return registers[0]
 
 def parse_input(lines: list[str]) -> tuple[list[list[str]], list[str]]:
     """parse input into sections"""
@@ -35,6 +53,27 @@ def parse_input(lines: list[str]) -> tuple[list[list[str]], list[str]]:
         index += 4
     index += 2
     return samples, list(lines[index:])
+
+def determine_opcodes(poperations) -> dict:
+    """determine operation codes by eliminating items appliciable to one code iteratively"""
+    operations = {}
+    while True:
+        todelete = []
+        for f, opcodes in poperations.items():
+            if len(opcodes) == 1:
+                code = list(opcodes)[0]
+                for f2, popcodes in poperations.items():
+                    if f == f2:
+                        continue
+                    if code in popcodes:
+                        popcodes.remove(code)
+                todelete.append(f)
+                operations[code] = f
+        for d in todelete:
+            del poperations[d]
+        if len(poperations) == 0:
+            break
+    return operations
 
 def match(f, before: list[int], expected: list[int], operation: str) -> int:
     """determine if the supplied function results in the expected register values"""
@@ -121,5 +160,4 @@ assert solve_part1(sample) == 1
 assert solve_part1(data) == 509
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(data) == 496
