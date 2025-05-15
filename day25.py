c2cfe2d2e982a688042fd5fe37e0ecb12601cb31
scottdharris <scottdharris11@yahoo.com>
2025-05-15 06:45:44 -0400

    day 25 setup

---
+++ day25.py
@@ -0,0 +1,16 @@
+"""utility imports"""
+from utilities.data import read_lines
+from utilities.runner import runner
+
+@runner("Day 25", "Part 1")
+def solve_part1(lines: list[str]) -> int:
+    """part 1 solving function"""
+    return 0
+
+# Data
+data = read_lines("input/day25/input.txt")
+sample = """""".splitlines()
+
+# Part 1
+assert solve_part1(sample) == 0
+assert solve_part1(data) == 0
